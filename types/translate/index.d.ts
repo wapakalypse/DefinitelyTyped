e9107839ee8081eb51e388b5f825a9501fd093d3
wapakalypse <mirovlad91@gmail.com>
2022-11-07 12:09:57 +0600

    @types/translate 1.4.2

--- types/translate/index.d.ts
+++ types/translate/index.d.ts
@@ -1,8 +1,13 @@
-// Type definitions for translate 1.4
+// Type definitions for translate 1.4.2
 // Project: https://github.com/franciscop/translate
 // Definitions by: Vladimir Kovpak  <https://github.com/wapakalypse>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function translate(text: string, lang: string): Promise<string>;
+interface TranslateOptions {
+    to?: string | undefined;
+    from?: string | undefined;
+}
+
+declare function translate(text: string, options?: TranslateOptions): Promise<string>;
 
 export = translate;
